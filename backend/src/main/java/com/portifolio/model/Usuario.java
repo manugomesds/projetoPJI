b7ffbaf556c8373274db3dfb890095d04dbb54c3
Diogo Mascarenhas <165600928+Broken157@users.noreply.github.com>
2026-06-01 01:19:07 -0300

    Update Usuario.java

--- backend/src/main/java/com/portifolio/model/Usuario.java
+++ backend/src/main/java/com/portifolio/model/Usuario.java
@@ -3,6 +3,8 @@ package com.portifolio.model;
 import com.portifolio.model.enums.TipoUsuario;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
