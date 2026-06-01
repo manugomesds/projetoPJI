1022ebd6aa10829a584b80480c37e415c9178f8a
Diogo Mascarenhas <165600928+Broken157@users.noreply.github.com>
2026-06-01 01:33:51 -0300

    Update Candidatura.java

--- backend/src/main/java/com/portifolio/model/Candidatura.java
+++ backend/src/main/java/com/portifolio/model/Candidatura.java
@@ -3,8 +3,8 @@ package com.portifolio.model;
 import com.portifolio.model.enums.StatusCandidatura;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -47,7 +47,7 @@ public class Candidatura {
     @Column(name = "link_portfolio_candidatura", nullable = false, length = 255)
     private String linkPortfolioCandidatura;
 
-    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private StatusCandidatura status;
 
